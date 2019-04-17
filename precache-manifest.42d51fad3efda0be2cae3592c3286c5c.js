9535c038dcb9aacf21447e4405b5fc6906ea4f72
Tyler Hunstock <Thunstock17@gmail.com>
2019-04-16 19:38:01 -0700

    Updates

---
+++ precache-manifest.42d51fad3efda0be2cae3592c3286c5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b5c36b7e36c7eab33d7",
+    "url": "/confusion-restaurant/static/css/main.be5e063b.chunk.css"
+  },
+  {
+    "revision": "4b5c36b7e36c7eab33d7",
+    "url": "/confusion-restaurant/static/js/main.4b5c36b7.chunk.js"
+  },
+  {
+    "revision": "d0de2f9ffd2c9c0d4b2a",
+    "url": "/confusion-restaurant/static/css/1.d44ac711.chunk.css"
+  },
+  {
+    "revision": "d0de2f9ffd2c9c0d4b2a",
+    "url": "/confusion-restaurant/static/js/1.d0de2f9f.chunk.js"
+  },
+  {
+    "revision": "f602c85a56afd6469ea2",
+    "url": "/confusion-restaurant/static/js/runtime~main.f602c85a.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion-restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion-restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion-restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion-restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion-restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0a164ec69eec66581ee041fb86f3799f",
+    "url": "/confusion-restaurant/index.html"
+  }
+];
